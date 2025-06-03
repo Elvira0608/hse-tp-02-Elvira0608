36c938b1813e0b9e725007d6bb995d6385f21c17
Elvira0608 <elvi0608>
2025-06-03 12:03:41 +0200

    CI tg2

---
+++ src/CINotify/bot.py
@@ -0,0 +1,32 @@
+import bot_config
+import telebot
+from pathlib import Path
+import json
+
+bot = telebot.TeleBot(bot_config.token)
+
+@bot.message_handler(content_types=["text"])
+def send_notification(status: bool):
+    if(status):
+        bot.send_message(bot_config.chat_id, bot_config.message_when_passed, "Markdown")
+    else:
+        bot.send_message(bot_config.chat_id, bot_config.message_when_failed, "Markdown")
+
+def check_test_status():
+    with open('../report.json', 'r') as file:
+        data = json.load(file)
+    #print(data)
+    failed_tests = int(data["report"]["summary"]["failed"])
+    if(failed_tests > 0):
+        print(data["report"]["summary"]["failed"])
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    #bot.infinity_polling()
+    if(Path("../report.json").is_file()):
+        if(check_test_status()):
+            send_notification(True)
+        else:
+            send_notification(False)
---
+++ src/CINotify/bot_config.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import json
+
+def get_report_when_failed():
+    temp = ""
+    with open('../report.json', 'r') as file:
+        data = json.load(file)
+
+    failed_tests = int(data["report"]["summary"]["failed"])
+    if(failed_tests > 0):
+        for test in data["report"]["tests"]:
+            if(test["outcome"] == "failed"):
+                temp += "*TEST NAME*: " + test["name"] + "\n"  
+                temp += test["call"]["longrepr"] + "\n"  
+        res = "*Feiled tests (%d)*\n" % failed_tests + temp
+    else:
+        res = "All passed"    
+    return res
+
+token = '7705354270:AAGKM_nf8lN12S7SZ90jcCkRl1Cr5bMzzC0'
+chat_id = '1331277451'
+message_when_passed = 'Tests complited succesfully'
+message_when_failed = get_report_when_failed()
